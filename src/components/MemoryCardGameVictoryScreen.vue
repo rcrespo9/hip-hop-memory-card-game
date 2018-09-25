fd05a627ea455707dc7db2090c025b60ad91df08
rcrespo9 <rudy.crespo20@gmail.com>
2018-09-25 15:10:10 -0400

    able to exit victory screen, started styling it

--- src/components/MemoryCardGameVictoryScreen.vue
+++ src/components/MemoryCardGameVictoryScreen.vue
@@ -1,28 +1,49 @@
 <template>
-  <div class="wrapper" :class="{ show: allPairsFound }">
-    <p>You did it! You won the game in {{finalTime}} with a {{successRate}} success rate!</p>
-    <button @click="startNewGame">Start New Game</button>
+  <div class="victory-screen" :class="{ show: this.allPairsFound && !victoryScreenClosed  }">
+    <div class="victory-screen__inner">
+      <p>You did it! You won the game in {{finalTime}} with a {{successRate}} success rate!</p>
+      <button @click="startNewGame">Start New Game</button>
+      <button @click="closeVictoryScreen">Exit</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MemoryCardGameVictoryScreen',
+  data () {
+    return {
+      victoryScreenClosed: false
+    }
+  },
   props: {
     allPairsFound: Boolean,
     finalTime: String,
     successRate: String,
     startNewGame: Function
+  },
+  methods: {
+    closeVictoryScreen () {
+      this.victoryScreenClosed = true;
+    }
   }
 }
 </script>
 
 <style scoped>
-  .wrapper {
+  .victory-screen {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 10;
     display: none;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
   }
 
   .show {
-    display: block;
+    display: flex;
   }
 </style>
