3a0f29e684a1d268da24ff6a24c8763b26215cdd
rcrespo9 <rudy.crespo20@gmail.com>
2018-09-25 08:59:05 -0400

    added background to front of cards

--- src/components/MemoryCardGameList.vue
+++ src/components/MemoryCardGameList.vue
@@ -15,6 +15,7 @@ export default {
   .memory-cards-list {
     display: grid;
     grid-template-columns: repeat(6, 1fr);
+    grid-row-gap: 1rem;
     list-style-type: none;
     margin: 0;
     padding: 0;
