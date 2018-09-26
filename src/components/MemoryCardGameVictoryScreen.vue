33011c362b71b534b74cb8fd91b6f3862467745b
rcrespo9 <rudy.crespo20@gmail.com>
2018-09-26 12:06:04 -0400

    gif background

--- src/components/MemoryCardGameVictoryScreen.vue
+++ src/components/MemoryCardGameVictoryScreen.vue
@@ -31,5 +31,9 @@ export default {
     align-items: center;
     width: 100%;
     height: 100vh;
+    background-image: url('https://media.giphy.com/media/9D5gC0ryykq6hx8Xri/giphy.gif');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
   }
 </style>
